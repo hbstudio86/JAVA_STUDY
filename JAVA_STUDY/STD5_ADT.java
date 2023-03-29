3525528a9035311d734da9691cd532bfbf87992d
HBSTUDIO <hbstudio@naver.com>
2023-03-29 18:42:03 +0900

    [ING] 추상화 인터페이스

---
+++ JAVA_STUDY/STD5_ADT.java
@@ -0,0 +1,22 @@
+abstract class Shape{
+    abstract public double getArea();
+}
+// 몸체 구현없이 형식만 존재하는 메소드
+// 객체 생성이 불가능하고 오직 파생만 할 수 있다.
+// 기능적으로 구현하기 어려운 메소드가 존재 : 하나의 함수에 여러가지 의미가 부여될 때, 즉 다의성 함수일 때
+
+// 인터페이스는 모든 메소드가 추상 메소드로 구성되어 있다.
+// 즉 일반 추상 클래스는 상속이 가능한 일반 메소드가 있고, 인터페이스는 100% 추상 메소드만 가짐
+// 데이터 필드는 클래스 상수만 가능(public static final)
+
+/**
+ * InnerSTD5_ADT*/
+public interface InerSTD5_ADT {
+    void if_view();
+}
+
+public class STD5_ADT {
+    static public void main(String[] args){
+
+    }
+}

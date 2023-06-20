b5188cd2be89ac452b5a0ae3385bd69647aa5554
HBSTUDIO <hbstudio@naver.com>
2023-06-20 22:19:43 +0900

    230620 연습

--- JAVA_STUDY/_000000_TOTAL_Practice.java
+++ JAVA_STUDY/_000000_TOTAL_Practice.java
@@ -1,3 +1,16 @@
+// java 종합적인 연습
+class SuperA{   // 시초
+    private int n1;
+    public SuperA(){this.n1 = 0;}   //필드 초기화
+    public SuperA(int n){this.n1 = n;}
+    public void showdata(){
+        System.out.println(this.n1);
+    }
+}
+
+
 public class _000000_TOTAL_Practice {
-    
+    public static void main(String[] args){
+
+    }
 }
